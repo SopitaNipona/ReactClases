82ae84d31be1392c8b99982d3b085abc397d0bf1
SopitaNipona <a01748692@tec.mx>
2024-04-11 13:41:42 -0600

    sets added with substract button

--- marcador-tenis/src/App.js
+++ marcador-tenis/src/App.js
@@ -12,22 +12,39 @@ function App() {
   const [puntosA, setPuntosA] = useState(0);
   const [puntosB, setPuntosB] = useState(0);
 
-  console.log("Puntos A dentro de App: ", puntosA);
+  const [setsA, setSetsA] = useState(0);
+  const [setsB, setSetsB] = useState(0);
+
+  //console.log("Puntos A dentro de App: ", puntosA);
 
   //Handler para el onClick de los botones
   const onClickHandlerA = () => {
-    console.log("Antes: ", puntosA);
+    //console.log("Antes: ", puntosA);
     setPuntosA(puntosA + 1);
-    console.log("Despues: ", puntosB) //Asincronos
+    //console.log("Despues: ", puntosB) //Asincronos
+  }
+
+  const onClickHandlerAmenos = () => {
+    if(puntosA > 0){
+      setPuntosA(puntosA - 1);
+    }
   }
 
   const onClickHandlerB = () => {
     setPuntosB(puntosB + 1);
   }
 
+  const onClickHandlerBmenos = () => {
+    if(puntosB > 0){
+      setPuntosB(puntosB - 1);
+    }
+  }
+
   const onClickHandlerReiniciar = () => {
     setPuntosA(0);
     setPuntosB(0);
+    setSetsA(0);
+    setSetsB(0);
   }
 
   return (
@@ -36,15 +53,16 @@ function App() {
       <Puntos puntos={puntosA} />
       <Titulo texto="Jugador B"/>
       <Puntos puntos={puntosB} />
-      <Titulo texto="Jugador A"/>
-      <Puntos puntos={puntosA} />
-      <Titulo texto="Jugador B"/>
-      <Puntos puntos={puntosB} />
 
-      <Boton texto="Gana punto A" onClick={onClickHandlerA}/>
-      <Boton texto="Gana punto B" onClick={onClickHandlerB}/>
-      <Boton texto="Gana punto A" onClick={onClickHandlerA}/>
-      <Boton texto="Gana punto B" onClick={onClickHandlerB}/>
+      <Titulo texto="Sets ganados de A"/>
+      <Puntos puntos={setsA} />
+      <Titulo texto="Sets ganados de B"/>
+      <Puntos puntos={setsB} />
+
+      <Boton texto="punto A +" onClick={onClickHandlerA}/>
+      <Boton texto="punto A -" onClick={onClickHandlerAmenos}/>
+      <Boton texto="punto B +" onClick={onClickHandlerB}/>
+      <Boton texto="punto B -" onClick={onClickHandlerBmenos}/>
       <Boton texto="Reiniciar" onClick={onClickHandlerReiniciar}/>
     </div>
   );
