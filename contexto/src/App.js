ee81a82627f5e5677a3302a770f0d3282321a721
SopitaNipona <a01748692@tec.mx>
2024-04-27 15:04:21 -0600

    dark background added

--- contexto/src/App.js
+++ contexto/src/App.js
@@ -3,16 +3,24 @@
 import './App.css';
 import ListaTareas from './components/ListaTareas';
 import ProveedorTarea from './components/ProveedorTarea';
+import { createContext,useState } from 'react';
 
-
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === 'light' ? 'dark' : 'light'));
+  };
   return (
-    <div className="App">
-      <ProveedorTarea>
+    <ThemeContext.Provider>
+    <div className="App" id={theme}>
+      <ProveedorTarea value={{theme, toggleTheme}}>
         <ListaTareas />
       </ProveedorTarea>  
     </div>
+    </ThemeContext.Provider>
   );
 }
 
